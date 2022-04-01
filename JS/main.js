d83aff2dae7ed016dd9d1f8bdb91afd933937c11
nitipat21 <nitipat.rstw@gmail.com>
2022-04-01 20:08:29 +0700

    create function reload data from localstorage

--- JS/main.js
+++ JS/main.js
@@ -8,5 +8,33 @@ const MAIN = (function(){
     STRING.autoComplete(DOM.searchBox,API.allCountriesName);
 
     DOM.submitBtn.addEventListener("click",API.getAllSearchResponse);
+
+    if (localStorage.length) {
+        if (localStorage.getItem("secondCountry")) {
+            const   secondCountry = LOCALSTORAGE.getLocalStorage("secondCountry"),
+                    secondCountryName = secondCountry[0],
+                    secondCountryCard = LOCALSTORAGE.getLocalStorage(secondCountryName),
+                    firstCountry = LOCALSTORAGE.getLocalStorage("firstCountry"),
+                    firstCountryName = firstCountry[0],
+                    firstCountryCard = LOCALSTORAGE.getLocalStorage(firstCountryName),
+                    bothCountries = LOCALSTORAGE.getLocalStorage("bothCountries"),
+                    compareList = LOCALSTORAGE.getLocalStorage("compareList");
+
+            DOM.createCountryCard(STRING.formatArraytoNumberWithCommas(Array.from(firstCountryCard)));
+            DOM.createCountryCard(STRING.formatArraytoNumberWithCommas(Array.from(secondCountryCard)));
+            DOM.generateCompareText(Array.from(bothCountries),Array.from(compareList));
+            DOM.showTextCompareResult();
+            DOM.searchBox.value = "";
+
+        } else {
+            
+            const   firstCountry = LOCALSTORAGE.getLocalStorage("firstCountry"),
+                    firstCountryName = firstCountry[0],
+                    firstCountryCard = LOCALSTORAGE.getLocalStorage(firstCountryName);
+
+            DOM.createCountryCard(STRING.formatArraytoNumberWithCommas(Array.from(firstCountryCard)));
+            DOM.searchBox.value = "";
+        }
+    }
     
 })();
